1a6c3a5816972073803f31f280c7b12699033a62
rriggio <rriggio@localhost.localdomain>
2013-03-04 16:12:01 +0100

    Use correct license

--- vtplanner/embedding/vtplanner.py
+++ vtplanner/embedding/vtplanner.py
@@ -5,18 +5,10 @@
 #
 # Copyright (C) 2013 Roberto Riggio <roberto.riggio@create-net.org>
 #
-# VTPlanner is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
 # VTPlanner is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>
+# COPYING file for more details.
 
 import os
 import subprocess 
